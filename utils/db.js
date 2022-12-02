f47660afe468c1c0c4603a3e9a69cf76985184f4
Marek <marek@macbook-air-marek.home>
2022-12-02 20:09:29 +0100

    first commit

--- utils/db.js
+++ utils/db.js
@@ -2,35 +2,35 @@ import mongoose from "mongoose";
 const connection = {}
 
 async function connect () {
-    console.log('start to connect in db');
-    if(connection.isConnected){
-        console.log('already connected');
-        return;
-    }
-    if(mongoose.connections.leghth > 0) {
-        connection.isConnected = mongoose.connections[0].readyState;
-        if (connection.isConnected ===1) {
-            connection.isConnected('use previous connection');
-            return;
-        }
-        await mongoose.disconnect();
-    }
+    // console.log('start to connect in db');
+    // if(connection.isConnected){
+    //     console.log('already connected');
+    //     return;
+    // }
+    // if(mongoose.connections.leghth > 0) {
+    //     connection.isConnected = mongoose.connections[0].readyState;
+    //     if (connection.isConnected ===1) {
+    //         connection.isConnected('use previous connection');
+    //         return;
+    //     }
+    //     await mongoose.disconnect();
+    // }
 
-    const db = await mongoose.connect(process.env.MONGODB_URI);
+    const db = await mongoose.connect('mongodb+srv://onestauser:8XwxABJyEETxygc7@onesta-base.evtzc20.mongodb.net/onesta-base?retryWrites=true&w=majority');
     console.log('new connection');
     connection.isConnected = db.connections[0].readyState;
     
 }
 
 async function disconnect() {
-    if(connection.isConnected) {
-        if(process.env.NODE_ENV === "production"){
-            await mongoose.disconnect();
-            connection.isConnected = false;
-        } else {
-            console.log('not connected')
-        }
-    }
+    // if(connection.isConnected) {
+    //     if(process.env.NODE_ENV === "production"){
+    //         await mongoose.disconnect();
+    //         connection.isConnected = false;
+    //     } else {
+    //         console.log('not connected')
+    //     }
+    // }
 }
 
 const db = {connect, disconnect};
