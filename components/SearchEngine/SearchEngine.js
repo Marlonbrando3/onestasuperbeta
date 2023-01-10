d1b615474f2c9b85a481cdfafe1014038ae8fe42
Marek <marek@macbook-air-marek.home>
2023-01-10 18:50:33 +0100

    first commit

--- components/SearchEngine/SearchEngine.js
+++ components/SearchEngine/SearchEngine.js
@@ -1,6 +1,6 @@
 import {useState, useContext, createContext} from "react";
-import { AppContext } from "../../pages/_app";
-import {useRouter} from 'next/router';
+// import { AppContext } from "../../pages/_app";
+// import {useRouter} from 'next/router';
 import SearchComponentsList from "./SearchComponentsList";
 import SearchResults from "./SearchResults";
 
