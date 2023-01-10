7cda3a31808b48a7c62fd2cfdea395707104aea7
Marek <marek@macbook-air-marek.home>
2023-01-10 18:37:53 +0100

    first commit

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext, createContext} from 'react'
 // import Head from 'next/head'
-// import {useRouter} from 'next/router'
+import {useRouter} from 'next/router'
 // import MiniHomeView from '../../components/SearchEngine/MiniHomeView'
 // import Header from '../../components/Header'
 // import SearchEngine from '../../components/SearchEngine/SearchEngine'
