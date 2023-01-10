a67bfc2eb48db63d1b22073effa8ad685e79ad79
Marek <marek@macbook-air-marek.home>
2023-01-10 19:26:30 +0100

    first commit

--- components/SearchEngine/SearchComponentsList.js
+++ components/SearchEngine/SearchComponentsList.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 // import CitySearch from "./CitySearch/CitySearch";
 // import RegionSearch from "./RegionSearch/RegionSearch";
-// import RegionChoosed from "./RegionSearch/RegionChoosed";
-// import TypeSearch from "./TypeSearch/TypeSearch";
+import RegionChoosed from "./RegionSearch/RegionChoosed";
+import TypeSearch from "./TypeSearch/TypeSearch";
 import TypeChoosed from "./TypeSearch/TypeChoosed"
 import CountrySearchInSearchEngine  from "../CountrySearchInSearchEngine"
 import Offersparameters from "./SearchparametrsCheckbox/Offersparameters"
