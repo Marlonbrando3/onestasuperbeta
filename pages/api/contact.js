3673ad8c401d980365f5bf2488a7056f1ffb8d55
Marek <marek@macbook-air-marek.home>
2023-01-11 22:09:25 +0100

    first commit

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,51 +1,46 @@
-// export default function (req, res) {
-//       console.log('jestem w API')
-//     require('dotenv').config()
-//     let nodemailer = require('nodemailer')
+export default function (req, res) {
+      console.log('jestem w API')
+    require('dotenv').config()
+    let nodemailer = require('nodemailer')
 
-//     const endEmail = 'biuro@onesta.com.pl';
-//     const fromEmail = "www@onesta.com.pl";
-//     const pass = 'Onesta_www_asks_123!'
+    const endEmail = process.env.END_MAIL;
+    const fromEmail = process.env.FROM_MAIL;
+    const pass = process.env.EMAIL_PASS
     
-//     const transporter = nodemailer.createTransport({
-//       port: 465,
-//       host: "mail-serwer141299.lh.pl",
-//       secure: true,
-//       // requireTLS: true,
-//       tls: {
-//         ciphers: "SSLv3",
-//       },
-//       auth: {
-//         type: "OAuth2",
-//         user: fromEmail,
-//         pass: pass,
-//       },   
-//       // dkim: {
-//       //   domainName: 'onesta.com.pl',
-//       //   keySelector: 'www@onesta.com.pl',
-//       // }
-//     })
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "mail-serwer141299.lh.pl",
+      secure: true,
+      tls: {
+        ciphers: "SSLv3",
+      },
+      auth: {
+        type: "OAuth2",
+        user: fromEmail,
+        pass: pass,
+      },   
+    })
 
-//     const mailData = {
-//       from: fromEmail,
-//       to: endEmail,
-//       subject: `Wiadomość ze strony od: ${req.body.Name}`,
-//       text: "Hello. This email is for your email verification.",
-//       html: 
-//       `Dotyczy ogłoszenia o nr. ref.${req.body.Id}`+`<br>`+
-//       `Email kontaktowy: ${req.body.Email}`+`<br>`+
-//       `Telefon kontaktowy: ${req.body.Phone}`+`<br><br>`+
-//       `Wiadomość ${req.body.Message}`+`<br>`
-//     }
+    const mailData = {
+      from: fromEmail,
+      to: endEmail,
+      subject: `Wiadomość ze strony od: ${req.body.Name}`,
+      text: "Hello. This email is for your email verification.",
+      html: 
+      `Dotyczy ogłoszenia o nr. ref.${req.body.Id}`+`<br>`+
+      `Email kontaktowy: ${req.body.Email}`+`<br>`+
+      `Telefon kontaktowy: ${req.body.Phone}`+`<br><br>`+
+      `Wiadomość ${req.body.Message}`+`<br>`
+    }
 
-//     transporter.sendMail(mailData, function (err, info) {
-//       console.log("wysłane")
-//       if(err)
-//         console.log(err)
-//       else
-//         console.log(info)
-//     })
-//     res.status(200)
-//   }
+    transporter.sendMail(mailData, function (err, info) {
+      console.log("wysłane")
+      if(err)
+        console.log(err)
+      else
+        console.log(info)
+    })
+    res.status(200)
+  }
 
   
