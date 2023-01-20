413794d4bb902408f50a9c0d90f61563a0eb3b61
Marek <marek@macbook-air-marek.home>
2023-01-20 11:43:36 +0100

    first commit

--- components/BlogItems.js
+++ components/BlogItems.js
@@ -7,14 +7,14 @@ export default function BlogItems() {
   return (
     <div className='flex justify-evenly flex-wrap w-10/12 mx-auto'>
         {Blog.map(obj => (
-            <div key={obj} className='lg:w-1/3 w-10/12 mx-auto py-6'>
-                <div className='w-10/12 mx-auto bg-gray-100/[0.2]'>
+            <div key={obj} className='lg:w-1/3 w-12/12 mx-auto py-2'>
+                <div className='w-10/12 mx-auto bg-gray-100/[0.4]'>
                     <Image className='h-52 object-cover'
                         src={obj.link}
                         width={500}
                         height={500}
                     />
-                    <div className='h-72 p-2'>
+                    <div className='p-2'>
                         <p className='font-bold text-xl'>{obj.title}</p>
                         <div>{obj.description}...</div>
                         <button className={obj.status !== "wating" ? 'border-gray-500 mt-12 px-3 py-1 rounded-lg cursor-pointer bg-white drop-shadow-md hover:bg-red-600 duration-100 hover:text-white': 'hidden'}><Link href={{pathname: `/blog/${obj.tag}`, query:{id: obj.id}}}>Czytaj więcej</Link></button>
