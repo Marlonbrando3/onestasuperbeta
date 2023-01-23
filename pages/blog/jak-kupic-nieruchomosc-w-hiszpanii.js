92c869f02cf71a9ea23001478c4cb0f149c1fdb4
Marek <marek@MacBook-Air-Marek.local>
2023-01-23 19:57:00 +0100

    first commit

---
+++ pages/blog/jak-kupic-nieruchomosc-w-hiszpanii.js
@@ -0,0 +1,131 @@
+import React from 'react'
+import ContactFormBlogPost from '../../components/ContactFormBlogPost'
+import HeaderBlog from '../../components/HeaderBlog'
+import HeaderBlogPost from '../../components/HeaderBlogPost'
+import MiniMainViewBlog from '../../components/MiniMainViewBlog'
+
+export default function BlogPost() {
+
+  const temat = "Jak kupić nieruchomość w Hiszpani (cz.1 określ swoje cele)"
+
+  return (
+    <>
+    <HeaderBlogPost temat={temat}/>
+<div className='lg:px-40 px-4 pt-24 leading-7 tracking-normal'>
+
+Zdecydowana większość osób rozglądająca się za swoją (zazwyczaj pierwszą) nieruchomością chce zrozumieć co wiąże się z samym zakupem, jaki jest proces ale na samym początku (to wynika z doświadczenia) wiele osób – bez urazy Drogi Czytelniku - nie do końca wie czego i po co szuka. <br></br><br></br>
+Często spotykamy się z bardzo szerokim spectrum poszukiwań, nasi rozmówcy zmieniają zdanie, regiony jakby za pstryknięciem palcem i jest to dla nas całkowicie zrozumiałe, ponieważ na początku drogi, poznawania i szukania co nam się podoba, a co nie – zupełnie normalnym jest, że podoba się pewnie wszystko w Hiszpanii. Brakuje metody, prowadzenia, która/które pomoże oszacować Państwa pragnienia, potrzeby, koncepcje.<br></br><br></br>
+Tym artykułem chcę spróbować temu zaradzić, dzieląc się też własnymi przykładami lub przykładami naszych klientów. W tym artykule (jako 2 częściach) skupimy się głównie na aspektach samego procesu określenia celu, metodyce wyboru miejsca i finalnie procesu zakupu nieruchomości w Hiszpanii lub domu w Hiszpanii, zatem pominięte zostaną aspekty traktujące o wyglądzie, wyposażeniu danej nieruchomości. Te aspekty zostaną opracowane w jednym z wpisów, które pojawią się na blogu niebawem.
+<br></br><br></br>
+W tej pierwszej części (tej którą czytasz) omawiamy metody, które skutecznie pomagają ubrać nasze zamysły w konkretne ramy przyczynowo skutkowe i określenie celu zakupu z jednej spośród 3 głównych opcji.
+Od tego powinno się zacząć jakiekolwiek kolejne poszukiwania.  
+<br></br><br></br>
+Zapraszam zatem do ułożenia sobie tego w głowie po pierwsze czytając poniższy wpis z uwagą i na końcu odpowiedzenie sobie na kilka pytań, które wylistowaliśmy aby ułatwić i ułożyć w głowie cały proces. .
+Naturalne stało się, że chcąc dokonać zakupu – chcemy (często samemu) dotrzeć do poziomu wiedzy, który da nam poczucie bezpieczeństwa i podczas rozmów z agencjami, doradcami poczucie, że wiemy na którym jesteśmy etapie i czy to co dana osoba do nas mówi wraz z oceną czy te informacje są kompletne i rzetelne – przydaje się nam się czy też nie.
+<br></br>
+Chciałbym aby ten artykuł dał Tobie czytelniku takie poczucie.
+<br></br><br></br>
+Zacznijmy.
+<br></br>
+Dlaczego chce kupić nieruchomość w Hiszpanii? Trywialne ale jak ważne pytanie. W swojej pracy dość często spotykam osoby, które precyzyjnie nie potrafią na to pytanie odpowiedzieć ponieważ ta myśl jest raczej na etapie wizji/marzenia. Co jest potrzebne do odpowiedzi? Należy zrozumieć jakie mamy cele. <br></br>
+Czy chcemy aby nasza nieruchomość w Hiszpanii była:<br></br>
+a) zakupowym typowo inwestycyjnym, czyli zrozumieliśmy lub jesteśmy w trakcie zdobywania wiedzy o atrakcyjności tej formy inwestowania i wybieramy zakup nieruchomości z późniejszym wynajmem całorocznym jako zdecydowanie lepszy substytut lokaty bankowej naszych środków finansowych,<br></br>
+<br></br>
+b) zakupem typowo prywatnym, miejsce które czeka na nas i co kilka, kilkanaście miesięcy jest przez nas odwiedzane,<br></br>
+<br></br>
+c) możemy również sprawę skomplikować i starać się połączyć oba powyższe interesy bazując na pewnym kluczu, który pozwoli nam oszacować potencjalne zyski z takiej inwestycji.<br></br>
+<br></br>
+Zatem po kolei aby dobrze to zrozumieć. 
+<br></br><br></br>
+Punkt a) „Interesuje mnie tylko zakup inwestycyjny, mam środki chcę aby pracowały najefektywniej”.
+<br></br>
+Jest naszą sytuacją wyjściową/bazową na jej podstawie będzie łatwiej mi w sposób jasny omówić pozostałe podpunkty. Osoba szukająca nieruchomości w Hiszpanii tak aby była ona wyłącznie lub głównie zakupem inwestycyjnym, czyt. „chcę kupić nieruchomość w Hiszpanii i wynajmować ją z jak największym zyskiem” powinna zwrócić uwagę na kilka podstawowych kryteriów:
+- odległość od morza (najlepiej do 1 km),<br></br>
+- posesja ma basen,<br></br>
+- 2 sypialnie,<br></br>
+- 2 łazienki,<br></br>
+- taras lub solarium ew. balkon,<br></br>
+- nieruchomość ma przydzielone miejsce parkingowe lub garaż,<br></br>
+- miejsce jest samowystarczalne (tj. mamy restauracje, market, park, przyjemną plażę),<br></br>
+- posesja jest bezpieczna (tj. ma monitoring, alarm, kod do bramy),<br></br>
+- dodatkowy atut - czy obiekt ma widom na morze (wpływa na cenę, nie zawsze to będzie atutem).<br></br><br></br>
+Te kryteria gwarantują bardzo dobry wynajem, wysokie obłożenie oraz zysk (czyli już po wszystkich podatkach, kosztach) oscylujący na poziomie 7% netto od kwoty netto nieruchomości.<br></br>
+Warunek? <br></br>
+Nieruchomość musi być gotowa/przeznaczona do wynajmu turystom w sezonie niskim i wysokim tj. okres od czerwca do (umownie) końca września. Innymi słowy – jeśli właściciel chce osiągnąć owo 7% nie powinien przebywać w swojej nieruchomości dłużej niż 2-3 tygodnie w sezonie. Oczywiście nie jest to zabronione, nieruchomość jest własnością kupującego i ma prawo być w niej cały rok jeśli zechce, natomiast jeśli ten czas będzie dłuższy niż wspomniane 2-3 tygodnie – zyski będą niższe z przyczyn oczywistych – nie wynajmujemy to nie zarabiamy, a o zyskach w tym punkcie mowa.
+Zatem przy wydłużeniu pobytu właścicielskiego trzeba być gotowym, że zysk spadnie do 6 lub 5%.<br></br>
+<br></br>
+Co jeszcze wypływa na rentowność?<br></br>
+<br></br>
+Odpowiedź jest bardzo prosta – odstępstwa od tego co zostało opisane w akapicie wyżej.
+Oddalimy się od morza, zabraknie miejsca parkingowego lub basenu – obiekt staje się mniej atrakcyjny, mniej użyteczny, tym samym cena za dany okres wynajmu spada wyraźnie. <br></br>
+<br></br>
+Przykład (z życia wzięty). Wynajęliśmy nieruchomość w Torrevieja w dobrej cenie. Cena była wyłącznym kryterium i tylko za pierwszym razem, o tym dlaczego piszę niżej. <br></br>
+<br></br>
+Szukaliśmy parkingu codziennie 15 - 20 minut plus czas dojścia do obiektu ok 7-10 minut. <br></br>
+<b>Czy wynajmę to miejsce jeszcze raz? </b><br></br><br></br>
+Raczej nie, chyba, że z sentymentu bo to był to piękny czas podczas pierwszej wizyty w Hiszpanii. Raczej będę szukał czegoś nieco droższego gdzie będę mógł zamiast spalać paliwo przez dodatkowo średnio 45 minut dziennie szukając miejsca, znaleźć coś z takim udogodnieniem jak miejsce parkingowe.<br></br><br></br>
+<b>Czy wystawię ocenę temu miejscu? </b><br></br>
+Nie zrobię tego ponieważ mam pewien swój sposób rozumienia owych ocen – czyli z mojej strony już nie będzie wyraźnej rekomendacji tego miejsca.
+<br></br><br></br>
+<b>Co jeśli ktoś wystawi ocenę? </b>
+Będzie to raczej ocena ze skali 5-7/10 czy taka ocena jest dobra? Booking jest wynajmowany najczęściej z ocenami 8/10. Zatem ocena 5-7 raczej zaszkodzi na tym portalu.
+
+Synteza - co się stanie z taką nieruchomością?
+Właściciel będzie musiał obniżyć ceny wynajmu, rentowność spadnie bo nie będzie tych ocen wielu,  a jak będą to raczej poniżej średniej. Z racji pewnych nieudogodnień (brak parkingu) mało kto będzie do niej wracał, więc ciągle będziemy musieli tę nieruchomość „dopalać” marketingiem. 
+<br></br><br></br>
+Czy brzmi to jak dobra inwestycja? <br></br>
+Bardziej nie, niż tak, prawda?
+<br></br><br></br>
+<b>Wniosek </b>– nie ulegajmy „dobrej cenie”, „okazjom”. Przestańmy wierzyć, że takie istnieją na tam mocno zagospodarowanym rynku. Nie jest ważne ile coś kosztuje ale jak szybko się zwróci oraz jak długo będzie rentowne.<br></br>
+<br></br>
+Zanim przejdziemy do punktu b), omówmy zagadnienie z punktu c). <br></br>
+<br></br>
+<b>Punkt c)  „Chcę wynajmować ale niech nich mi się też podoba bo będę tam na wakacje”</b><br></br>
+Czyli w jednak chcę aby mi się ta nieruchomość podobała, aby wakacje w tym apartamencie należały do tych dobrych i abym czuł się dobrze ale nich ona też zarabia, niech się zwraca, niech jest to mądry zakup. Jeśli myślisz o zakupie w taki sposób to aby określić z dużą pewnością, że  poszukiwany apartament w Hiszpanii mieć nie 2 nie 3 łazienki, oraz 40 m2 tarasu jest trudne prawda?  Dlatego idźmy dalej.<br></br>
+<br></br>
+To wyżej nazywamy sytuacją „pół na pół” gdzie mimo tego, że są to te 2-3 tygodnie (niektórzy planują też przenieść swoje życie za kilka, kilkanaście lat do tej nieruchomości w Hiszpanii) staję się to równie ważne co zarobki z wynajmu.<br></br>
+<br></br>
+Często się zdarza, że nieruchomości, które są atrakcyjne w takich kwestiach już pro-właścicielskich (np. chce mieć nieco większy ogród bo zawsze o takim marzyłem/marzyłam aby móc rozstawić stół  na trawniku na 8 osób oraz przy okazji mieć mały basen i palmę, która nie jest tam wciśnięta na siłę) mogą być nieco oddalone od plaży, np. jest to 1,6 km (co już obniża potencjał z wynajmu) ale jednak żadna inna nieruchomość nie trafiła moje w gusta. <br></br>
+Co wtedy?<br></br>
+Wtedy trzeba postawić na szali słabszy zarobek (jednak jest do daleko już raczej na stałe czyli, już te 7% nie będzie osiągalne, a 6 może 5% i nawet mniej jeśli danemu obiektowi czegoś jeszcze zabraknie (załóżmy nie będzie basenu) i pogodzić się, że praktycznie zawsze będzie już ten zarobek niższy o te 1-3%. Tutaj zawsze potrzebna jest rozmowa, oszacowanie ceny nieruchomości, potencjału danej lokalizacji oraz decyzja czy moje pragnienie posiadania „dokładnie takiego ogrodu” jest warte straty załóżmy 2% przychodów rocznie rok do roku.<br></br>
+<br></br>
+<b>Punkt b) „Szukam dla siebie”</b>
+<br></br>
+Czyli kupuję po to abym mieszkał tam, odwiedzał to miejsce wyłącznie ja lub moja rodzina. Sytuacja jest najprostsza z punktu widzenia samego procesu decyzyjnego nas jako agencji prowadzącej, niemniej trudna z dla osoby której pomagamy. Gusta są różne i albo trafimy albo nie. Tutaj naszym zadaniem w procesie jest praktycznie próba bardzo wysokie poziomu zrozumienia tego o czym mówi do nas nasz przyszły klient i oferowanie tego co jest temu najbliższe wraz z etapowym wykluczaniem ofert nietrafionych (lub elementów nietrafionych w tych ofertach) oraz wybieraniem aspektów, które są w gustach klienta i przy których zostajemy. Osoby szukające nieruchomości „dla  siebie” mają też swoje kryteria odległości od plaży. Najczęściej (choć nie jest to jakąś regułą) oddalamy się od morza znacznie, już w kilometrach liczmy tę odległość. Naturalnie bo blisko plaży w sezonie raczej nie doznamy spokoju a owo 10 km od morza już tak z opcją dojazdu w ciągu 10 min do plaży. Nieruchomości tak oddalone od plaży mają również znacznie niższą cenę czasami nawet 2-krotnie i ich jakość jest dobra. Natomiast tutaj nie ma mowy o skutecznym wynajmie i zarabianiu, można po prostu zmniejszać koszty takiej nieruchomości lub wychodzić na przysłowiowe „zero”. Jeśli nieruchomość jest kupiona za gotówkę to będzie to jakiś przychód, niemniej trudny do oszacowania na oko.
+
+Jaki region?
+
+Jeśli zależy mi na regionie to gównie wybór raczej pada między Costa del Sol (Malaga) oraz Costa Blanca (Alicante).
+
+Jakie mamy główne różnice?
+
+Ceny nieruchomości na Costa del Sol są ok 30% wyższe niż na Costa Blanca,
+zyski wyrażone w % są raczej podobne,
+koszty życia są ok 19% wyższe na Costa del Sol w niż na Costa Blanca,
+Costa del Sol jest regionem bardziej deszczowym przez to bardziej zielonym (ładniejszym) ale też nieco trudniejszym w zaplanowaniu wynajmu (deszcze),
+Costa del Sol jest regionem nieco chłodniejszym niż Costa Blanca.
+
+
+Po przeczytaniu tej części – zadaj sobie pytania:
+1. Czy chcę kupić coś inwestycyjnie czy tylko dla siebie lub w opcji „pół na pół”.
+2. Costa Blanca czy Costa Brava ? Który region jest bliższy?
+3. Jak bardzo chcę aby nieruchomość spełniała moje prywatne oczekiwania?
+4. Czy jestem gotowy aby nieruchomość zarabiała mniej na rzecz spełnienia moich gustów?
+5. Jeśli powyższa odpowiedź brzmi „tak” to ile % zysku (startujemy od 7%) warte są moje marzenia? Na ile jestem gotów?
+6. Jak blisko chce mieć do plaży?
+7. Czy chcę i czy muszę mieć blisko restauracje?
+8. Czy chce i czy muszę mieć blisko market?
+9. Czy chcę i czy muszę mieć widok morze?
+10. Czy aby mieć te 3 atuty wyżej jestem w gotowy zwiększyć swój budżet?
+11. Czy interesuje mnie nowe budownictwo czy rynek wtórny?
+12. Czy chcę mieć tę nieruchomość szybko czy chcę poczekać na nowe lokale?
+13. Ile chcę mieć sypialni i łazienek?
+14. Interesuje mnie (uszereguj lub wyklucz) apartamentowiec, dom, bungalow.
+
+
+Zapraszamy Państwa do części drugiej, która ukaże się na dniach i będzie traktować o procesie.
+Daj znam znać jeśli chcesz kontynuować rozmowę z nami, uzupełniając formularz.
+</div>
+<ContactFormBlogPost />
+</>
+)}
