eb225bc3b9a0a75a414871e7c8ce9b7b913215bf
Marek <marek@MacBook-Air-Marek.local>
2023-01-26 10:38:37 +0100

    first commit

--- pages/zarzadzanie-nieruchomosciami-w-hiszpanii.js
+++ pages/zarzadzanie-nieruchomosciami-w-hiszpanii.js
@@ -30,52 +30,54 @@ Artykuł przygotowany dla osób poszukujących apartamentu na sprzedaż w Hiszpa
 
 <div className='border my-10 px-2 lg:w-2/3 w-full'>
     <p className='py-2 font-bold text-xl'>Spis treści:</p>
-<p className='py-'>1. Czym jest zarządzanie nieruchomości w Hiszpanii?</p>
+<p className='py-1'>1. Czym jest zarządzanie nieruchomości w Hiszpanii?</p>
 <p className='py-1'>2. Najlepsze regiony pod kątem zarządzania nieruchomościami w Hiszpanii.</p>
 <p className='py-1'>3. Jak działa zarządzanie nieruchomościami w Hiszpanii?</p>
 <p className='py-1'>4. Jak rozliczane jest zarządzanie nieruchomościami w Hiszpanii?</p>
-<p className='py-1'>5. Rozliczenie i zyski</p>
+<p className='py-1'>5. Dochód, zysk, zwrot z inwestycji</p>
 <p className='py-1'>6. Zależności sezonowe</p>
 </div>
 
 <b className='text-xl'>1. Czym jest zarządzanie nieruchomościami w Hiszpanii?</b><br></br><br></br>
 
-Dla osób, które pierwszy raz spotykają się z terminem zarządzanie nieruchomościami w Hiszpanii lub innych krajach wyjaśniamy po krótce model, który jest bardzo prosty i polega na krótko terminowym (średnio 7 dni)  wynajmie zakupionej nieruchomości turystom. Z racji faktu, że wynajem jest dość kosztowny, korzystają z tej formy bardziej zamożni klienci co jest też swoistą gwarancją, że słowni poważni ludzi zawitają w naszej nieruchomości nie zniszczą jej wyposażenia.
-Kryteria do spełnienia aby zarządzanie nieruchomością było efektywne.<br></br><br></br>
- 
-Wcześniejszy krok, polega na mądrym wyborze nieruchomości w Hiszpanii do późniejszego wynajmu pod (więcej o tym jak wybierać w znajdziesz w artykule  <Link href="/jak-kupic-nieruchomosc-w-hiszpanii" className='font-bold text-blue-600 underline'>Jak kupić nieruchomość w Hiszpanii (cz.1 Określ swoje cele) </Link>).  
-Przy wyborze opieramy się o kryteria jakie ma spełniać nieruchomość tj. odległość od morza, atuty, liczba danych pomieszczeń itd. Owe kryteria są podyktowane potrzebami rynku i tego czego szukają co cenią i do czego wracają nasi turyści z wiarą iż staną się Oni stałymi lub polecającymi innym naszą nieruchomość klientami.<br></br><br></br>
+Dla osób, które pierwszy raz spotykają się z terminem zarządzanie nieruchomościami w Hiszpanii wyjaśniamy po krótce mode. Jest on bardzo prosty i polega na krótkoterminowym (średnio 10 dni) wynajmie wcześniej zakupionej nieruchomości - turystom, którzy bardziej szukają komfortu i wymarzonego miejsca do odpoczynku niż dobrej ceny. <br></br><br></br>
 
-Rzecz jasna aby ten model zarządzania nieruchomościami w Hiszpanii (od której zaczęliśmy) lub innych krajach z naszego portfolio dział trzeba spełni szereg wymogów i standardów jakościowych, ponieważ promowanie i dbanie nieruchomości oraz jej popularność jest zdecydowanie maratonem niż sprintem. <br></br><br></br>
+Nieruchomości są standardu wysokiego, cena wynajmu jest również wyższa niż średniej rynkowa i korzystają z tej formy bardziej zamożni klienci co jest też swoistą gwarancją, że to słowni i poważni ludzie zawitają w naszej nieruchomości nie zostawiając po sobie zniszczeń. Finalnie z tego zestawienia zarobek na wynajmnie jest wysoki.<br></br>
+ 
+Wcześniejszy krok - jak to zostało nadmienione - polega na mądrym wyborze nieruchomości w Hiszpanii do późniejszego wynajmu pod (więcej o tym jak wybierać nieruchomości (pod kątem inwestycyjnym ale również dla siebie) w znajdziesz w artykule  <Link href="/jak-kupic-nieruchomosc-w-hiszpanii" className='font-bold text-blue-600 underline'>Jak kupić nieruchomość w Hiszpanii (cz.1 Określ swoje cele) </Link>). <br></br><br></br>
+Przy wyborze nieruchomości opieramy się o kryteria jakie ma owa spełniać tj. odległość od morza, atuty w postaci solarium, basenu, garażu oraz liczby pomieszczeń itd. Owe kryteria są podyktowane potrzebami rynku i tym czego szukają, co cenią i do czego wracają nasi Goście z wiarą i celem iż staną się Oni Goścmi stałymi lub polecającymi - bliskim lub przez pozytywne oceny - naszą nieruchomość.<br></br><br></br>
+
+Rzecz jasna aby ten model zarządzania nieruchomościami w Hiszpanii (lub jakimkolwiek innym kraju) działał trzeba stale spełniać szereg wymogów, standardów jakościowych, znać i utrzymywać dobre praktyki ponieważ promowanie i dbanie o wynajmowane nieruchomości oraz ich popularyzowanie jest zdecydowanie maratonem niż sprintem. Sprintem wręcz zrobimy sobie krzywdę m.in. szukając okazji również. Należy pamiętać, że okazja najczęściej zaczyna się i kończy przy cenie początkowej nieruchomości bez brania pod uwagę wszystkiego co następuje lub jakie ma, ponieważ na tle tej wiedzy to już przestaje być okazją. Najbezpieczniej wyzbyć się słowa &quot;okazja&quot;. Jeśli brzmi to kontrowersyjnie, szczególnie dla osób zaczynających swoją przygodę - zapraszamy do kontaktu z naszymi expertami w tej sprawie celem polemiki. <br></br><br></br>
 
 <b className='text-xl'>2. Najlepsze regiony pod kątem zarządzania nieruchomościami w Hiszpanii</b><br></br><br></br>
 
-Współpracujemy wiele lat z partnerami, którzy otrzymują wyróżnienia na poziomie jakości oraz sprawności zarządzania nieruchomościami w Hiszpanii, zaczynami również procesy zarządzania nieruchomościami w Chorwacji oraz Portugalii. Nasi Hiszpańscy partnerzy bazują  na swoim wieloletnim doświadczeniu dowodząc, że skuteczność potrafi być bardzo wysoka i można zdobyć rynek wysoką jakością nie tylko napompowaną reklamą.<br></br><br></br>
+Współpracujemy wiele lat z Partnerami, którzy otrzymują wyróżnienia jakościowe oraz w sprawności zarządzania nieruchomościami w Hiszpanii. Nasi Hiszpańscy wybitni partnerzy bazują na swoim wieloletnim doświadczeniu dowodząc, że skuteczność potrafi być bardzo wysoka i można zdobyć rynek wysoką jakością, poleceniami, ocenami zamiast nachalną reklamą. Obecnie 70% Gości to osoby wracające, drugi i kolejne razy. <br></br><br></br>
 
-Działamy głównie na wybrzeżu Costa Blanca, rozwijając również bezpiecznie Costa del Sol. Oba regiony są wysoce atrakcyjne jeśli chodzi o inwestowanie, niemniej poszukując apartamentów na sprzedaż w Hiszpanii warto skupić swoją uwagę na tym pierwszym wybrzeżu, czyli nieruchomościach na sprzedaż na Costa Blanca z kilku wyraźnych powodów:<br></br><br></br>
-    • stabilniejsza i mniej deszczowa pogoda, która rzadko zawodzi turystów, jest dla Nich bezpieczniejsza dlatego klienci cześciej wracają na Costa Blanca,<br></br>
+Wraz z Partnerami działamy głównie na wybrzeżu Costa Blanca, rozwijając również bezpiecznie Costa del Sol. <br></br>
+Oba regiony są wysoce atrakcyjne jeśli chodzi o inwestowanie, niemniej poszukując apartamentów na sprzedaż w Hiszpanii warto skupić swoją uwagę na tym pierwszym wybrzeżu, czyli nieruchomościach na sprzedaż na Costa Blanca z kilku wyraźnych powodów:<br></br><br></br>
+    • stabilniejsza i mniej deszczowa pogoda, która rzadko zawodzi turystów, jest dla Nich bezpieczniejsze dlatego klienci cześciej wracają na Costa Blanca,<br></br>
     • niże ceny apartamentów o średnio ok 30% w porównaniu do Costa del Sol,<br></br>
     • ceny życia na Costa del Sol są wyższe średnio 19% w porównaniu do Costa Blanca,<br></br>
-    • region jest stabilny i „doświadczony” przez wiele lat zarządzania.<br></br><br></br>
+    • region jest stabilny i &quot;przećwiczony&quot; przez wiele lat zarządzania.<br></br><br></br>
 
-To są główne atuty i jeśli myślimy inwestowaniu w sposób bezpieczny i oparty o wiele przećwiczonych sytuacji Costa Blanca jest idealna. Wielu naszych klientów szuka jednak czegoś w klimatach bardziej zielonych, kolorowych i wtedy Marbella, Estepona (Costa del Sol) są często spełnieniem ich marzeń w te regiony także zapraszamy. Popularność jednego i drugiego jest ogromna, a różnice głównie w osobistych zamiłowaniach oraz w zamożności klientów, Costa del Sol jest tą droższą opcją.<br></br><br></br>
+To są główne atuty i jeśli myślimy inwestowaniu w sposób bezpieczny i oparty o wiele przećwiczonych sytuacji - Costa Blanca jest idealna. Wielu naszych klientów szuka jednak czegoś w klimatach bardziej zielonych, kolorowych i wtedy Marbella, Estepona (Costa del Sol) są często spełnieniem marzeń,  w te regiony także zapraszamy. Popularność jednego i drugiego jest ogromna, a różnice głównie w osobistych zamiłowaniach oraz w zamożności klientów, Costa del Sol jest tą droższą opcją zarówno w zakupie, wynajmie jak i codziennym życiu.<br></br><br></br>
 
 <b className='text-xl'>3. Jak działa zarządzanie nieruchomościami w Hiszpanii?</b><br></br><br></br>
 
-Firma, która podejmuje się zarządzania naszą nieruchomością jest odpowiedzialna ogólnie rzecz biorąc za przygotowanie nieruchomości oraz „wymianę gości” zatem:<br></br><br></br>
+Firma, która podejmuje się zarządzania zakupioną nieruchomością w Hiszpanii jest odpowiedzialna ogólnie rzecz biorąc za przygotowanie nieruchomości oraz „wymianę gości” zatem:<br></br><br></br>
 
     • umeblowanie nieruchomości po jej zakupie,<br></br>
     • każdorazowe przygotowanie lokalu na przyjęcia gości,<br></br>
     • sprzątanie lokalu tj. serwis, pranie, wszelkie mycie oraz przygotowanie pakietów startowych dla kolejnych gości,<br></br>
     • przyjęcie i rejestracja gości,<br></br>
-    • krótki ale ważny instruktaż i omówienie dobrych i skutecznych praktyk w ew. sytuacjach awaryjnych,<br></br>
+    • krótki ale ważny instruktaż i omówienie dobrych oraz skutecznych praktyk w ew. sytuacjach awaryjnych,<br></br>
     • wymeldowanie gości.<br></br><br></br>
 
-Cały proces ma gwarantować sprawne i rzeczowe reagowanie, gwarantować komfort, a cały system ma być dla gości bezproblemowy zakończony pozytywną opinią o nieruchomości, dalszymi poleceniami oraz rekomendacjami. To właśnie z rekomendacji pochodzi aż 70% klientów wynajmujących, a jest to efektem skrupulatnych i długoterminowych działań Naszych Partnerów.<br></br><br></br>
+Cały proces gwarantuje sprawne i rzeczowe reagowanie, komfort. Praktyki są dla Gości bezproblemowe, a pobyt zakończony pozytywną opinią o nieruchomości, dalszymi poleceniami oraz rekomendacjami z ktrych jak już wspomniałem pochodzi aż 70% Gości wynajmujących.<br></br><br></br>
 
 <b className='text-xl'>4. Jak rozliczane jest zarządzanie nieruchomościami w Hiszpanii?</b><br></br><br></br>
 
-Najlepiej wyjaśnić to na przykładzie pochodzącym od naszych partnerów z naszym delikatnym komentarzem jak przystało na wpisy blogowe.<br></br><br></br>
+Najlepiej wyjaśnić to na przykładzie pochodzącym od naszych Partnerów z delikatnym komentarzem jak przystało na wpisy blogowe.<br></br><br></br>
 
 4.1 <b>Koszty właściciela:</b><br></br><br></br>
 &nbsp; 4.1.1 <b>Usługa zarządzania nieruchomością 16% plus VAT</b> - (podatek w Hiszpanii wynosi 21%, czyli brutto 19,36%) od kwoty wynajmu w skład tego wchodzą:<br></br>
@@ -115,12 +117,14 @@ Co daje ok 64 500 zł zarobku rocznie.</b><br></br><br></br>
 
 <b className='text-xl'>6. Zależności sezonowe</b><br></br><br></br>
 
-Ceny wynajmu uzależnione są od okresu roku. W sezonie wysokim (lipiec, sierpień) te kwoty są najwyższe. W miarę zbliżania się do owego sezonu ceny rosną, w miarę oddalania się od owego – ceny maleją. W okresach poza sezonem nieruchomości wynajmowane są lokalnym nieco bardziej zamożnym Hiszpanom (65 % klientów to Hiszpanie), którzy chcą spędzić zimę bliżej morza. W sezonie ceny wynajmu są ok 2,5 - krotnie wyższe niż w okresie zimy.<br></br><br></br>
+Rożróżnijmy 3 główne okresy, które pomogą nam w przysfojeniu wiedzy: sezon wysoki, sezon niski, okres zimowy lub pozasezonowy.
+
+Ceny wynajmu uzależnione są od okresu roku. W sezonie wysokim (lipiec, sierpień) te kwoty są najwyższe. W miarę zbliżania się do owego sezonu z okresu zimowego-  ceny rosną, w miarę oddalania się od owego – ceny maleją. W okresach poza sezonem nieruchomości wynajmowane są lokalnym, nieco bardziej zamożnym Hiszpanom (65 % klientów to Hiszpanie), którzy chcą spędzić zimę bliżej morza. W sezonie ceny wynajmu są ok 2,5 - krotnie wyższe niż w okresie zimy.<br></br><br></br>
 
 W sezonie niskim (czerwiec i wrzesień) oraz wysokim (lipiec  i sierpień) obłożenie jest bliskie 100% - zależy to od regionu i znów gdy oddalamy się od tego okresu zaczyna ono wyraźnie spadać. Trudno oszacować dokładnie jak będzie ono wyglądało uśrednione w skali roku ponieważ  zmiennych wpływających jest bardzo dużo. Niektóre obiekty – choć sporadycznie – cieszą się obłożeniem nawet 100% w roku, raczej powinniśmy przyjąć średnio 60% rocznie, jak będzie więcej to będzie to dobrym powodem do radości.<br></br><br></br>
 
 
-W trakcie użytkowania lokalu mogą i prędzej czy później pojawią się dodatkowe koszty występujące dość naturalnie  - napraw, wymian zużytych np. pościeli lub odświeżenia lokalu itp. które stanowią koszt dodatkowy/alternatywny w owej inwestycji.<br></br><br></br>
+W trakcie użytkowania lokalu prędzej czy później pojawią się dodatkowe koszty występujące dość naturalnie z tyt. napraw, wymian zużytych np. pościeli lub odświeżenia lokalu poprzez odmalowanie itp. które stanowią koszt dodatkowy/alternatywny w owej inwestycji.<br></br><br></br>
         </div>
         <ContactFormBlogPost temat="Zakładka - Zarządanie nieruchomościamu"/>
         <br></br><br></br>
