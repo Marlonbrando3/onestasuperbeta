b935930da2c7c860c346ad2280921df50182b5ae
Marek <marek@MacBook-Air-Marek.local>
2023-02-15 18:11:59 +0100

    first commit

--- components/Header.js
+++ components/Header.js
@@ -146,12 +146,11 @@ export default function Header({}) {
                       <li className='list'><Link href='/abc'>ABC Inwestora</Link></li>
                           <li className='list'>Kontakt</li>
                       </ul>
-                      <div className="h-12/12 flex justify-end items-center lg:hidden">
+                      <div className="h-full w-full flex justify-end items-center lg:hidden">
                         {MobileMenu ? <CloseIcon 
                           className="transition-all duration-700 cursor-pointer block z-50 w-10 h-10 lg:hidden visible"
                           onClick={handleShowMobileMenu}/> : 
-                          <MenuIcon 
-                          className="transition-all duration-700 cursor-pointer z-50 w-10 h-10 lg:hidden block"
+                          <MenuIcon className="transition-all duration-700 cursor-pointer z-50 w-10 h-full lg:hidden block"
                           onClick={handleShowMobileMenu}/>}
                       </div>
                 </div>
