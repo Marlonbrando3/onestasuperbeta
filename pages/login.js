a60f73bbaa412824829087556e6dcd11f3a39430
Marek <marek@MacBook-Air-Marek.local>
2023-02-24 17:19:07 +0100

    first commit

--- pages/login.js
+++ pages/login.js
@@ -14,7 +14,7 @@ const [comm, setComm] = useState();
 const handleLogin = async(req,res) => {
     console.log(email.current.value+" with pass: "+pass.current.value)
 
-    let ref = await fetch('/api/panel', {
+    let ref = await fetch('/api/login', {
         method: 'POST',
         model:"no-cors",
         headers: {
